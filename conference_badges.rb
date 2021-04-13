bc58dae7368fdfbc3c05189379f22fb050bfe8af
Halie Hall <halierae17@outlook.com>
2021-04-13 17:54:07 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,7 +3,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each do |me|
-    puts "Hello, my name is #{me}."
+  attendees.each do |name|
+    badge_maker(name)
   end
 end
